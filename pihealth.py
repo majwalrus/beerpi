5067c74d1ddfa733a5474a7e5e4a066eb7d6de05
majwalrus <jameshiggin@doctors.net.uk>
2019-05-05 00:02:23 +0100

    forgot to add import os and also update comments section

--- pihealth.py
+++ pihealth.py
@@ -1,12 +1,15 @@
+# Class PiHealth
+#  This is for monitoring the health of the raspberry pi, so that it can be shutdown
+#  if there are problems such as overheating.
 
-
+import os
 
 class PiHealth:
 
     piTemp=0
     piTempStr=""
 
-    def getPiTemp(self):
+    def getPiTemp(self):    # Uses vcgencmd to obtain the Pi's temperature
         self.piTemp=os.popen("vcgencmd measure_temp").readln()
         self.piTemp=self.temp.replace("temp=","")
         self.piTempStr=str(self.piTemp)
