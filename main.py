af4b2ac40e949a0c087a50f7488d6d6bdf4953c6
BuildTools <unconfigured@null.spigotmc.org>
2019-05-23 13:03:56 +0100

    added sensors config menu

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ class BeerSensors(Screen):
 
     def update(self,dt):
         for  tmp_probe in glob_beerProbes.probeList:
-            tmp_LabelVal=str(tmp_probe.name)+" T: "+tmp_probe.probevalstr
+            tmp_LabelVal=str(tmp_probe.name)+" T: "+str(tmp_probe.probeval)
             #self.probeLabelValue.append(tmp_LabelVal)
         pass
 
@@ -130,7 +130,7 @@ class BeerSensors(Screen):
         num=0
         #self.probeLabelValue.clear()
         for  tmp_probe in glob_beerProbes.probeList:
-            tmp_LabelVal=str(tmp_probe.name)+" T: "+tmp_probe.probevalstr
+            tmp_LabelVal=str(tmp_probe.name)+" T: "+str(tmp_probe.probeval)
             self.probeLabelValue.append(tmp_LabelVal)
             self.add_widget(Label(text=self.probeLabelValue[num], top=self.top + 160 - (num*30),x=self.x-220))
             num+=1
