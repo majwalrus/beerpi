04eac66e859423aaed83e79b7f676c237f75923a
James Higgin <jameshiggin@doctors.net.uk>
2019-05-27 16:12:36 +0100

    added element classes and configuration

--- main.py
+++ main.py
@@ -251,7 +251,7 @@ def elementThreadControl(): #   Actions element class
     while True:
         print "Check Element Thread\n\r"
         checkElementData()
-        if not glob_beerProbes.returnStrProbeValFromName(glob_config.sensorHLT)=="false":
+        if not (glob_beerProbes.returnStrProbeValFromName(glob_config.sensorHLT)=="" or glob_beerProbes.returnStrProbeValFromName(glob_config.sensorHLT)=="false"):
             print glob_beerProbes.returnStrProbeValFromName(glob_config.sensorHLT)+" : "+str(timer)+"\n"
             glob_hltElement.elementControl(int(glob_beerProbes.returnStrProbeValFromName(glob_config.sensorHLT)),timer)
         #if not glob_beerProbes.returnStrProbeValFromName(glob_config.sensorBoil)=="false":
