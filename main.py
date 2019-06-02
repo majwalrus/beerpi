9d0b24050a417b614362a2cc1f6b2636f2d3f4f5
James Higgin <jameshiggin@doctors.net.uk>
2019-06-02 17:15:37 +0100

    added shutdown option
    Signed-off-by: James Higgin <jameshiggin@doctors.net.uk>

--- main.py
+++ main.py
@@ -143,6 +143,10 @@ class BeerOff(Screen):     # Power off screen
         self.menu = DefaultRightBar()
         self.add_widget(self.menu)
 
+    def confirmShutdown(self,num, *args): # function that is called when YES is clicked on shutdown screen
+        pass
+
+
 class BeerConfig(Screen):   # Main config screen, very little on it but does have the config menu to the right
 
     def update(self, dt):
