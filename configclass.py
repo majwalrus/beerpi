b6b6ea3ba72acc9848a0ceaa35b33cd517ca6c63
James Higgin <jameshiggin@doctors.net.uk>
2019-07-13 14:36:54 +0100

    hunting element control bug
    Signed-off-by: James Higgin <jameshiggin@doctors.net.uk>

--- configclass.py
+++ configclass.py
@@ -68,7 +68,7 @@ class BeerConfig:
 
         self.config.add_section("Boil")             # Boil value section
         self.config.set("Boil","targettemp","100")
-        self.config.set("Boil","tapertemp","97")
+        self.config.set("Boil","tapertemp","94")
         self.config.set("Boil","gpio","5")
 
         self.config.add_section("Sensors")          # Sensors  value section
